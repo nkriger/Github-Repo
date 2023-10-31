5ed909cf90c664fc0b7e540c947d713afabc8ba3
nkriger <n8@thekrigers.com>
2023-10-31 11:50:55 -0700

    aasdasd

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 30f;
+    float cameraRotationSpeed = 100f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void camera()
+    {
+        GetKey
+    }
+}
